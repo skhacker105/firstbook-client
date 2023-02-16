298d15225f0a4e237b484f4be43012775570a967
skhacker105 <skhacker105@gmail.com>
2023-02-16 20:34:55 +0530

    issue fix

--- src/app/core/resolvers/load-user-product.resolver.ts
+++ src/app/core/resolvers/load-user-product.resolver.ts
@@ -5,6 +5,7 @@ import {
   ActivatedRouteSnapshot
 } from '@angular/router';
 import { map, Observable, of, tap } from 'rxjs';
+import { HelperService } from '../services/helper.service';
 import { ProductService } from '../services/product.service';
 import { UserService } from '../services/user.service';
 
@@ -13,9 +14,14 @@ import { UserService } from '../services/user.service';
 })
 export class LoadUserProductResolver implements Resolve<boolean> {
 
-  constructor(private userService: UserService, private productService: ProductService) { }
+  constructor(
+    private userService: UserService,
+    private productService: ProductService,
+    private helperService: HelperService
+    ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    if (!this.helperService.isLoggedIn()) return of(true)
     return this.productService.userProducts()
       .pipe(
         tap(res => {
